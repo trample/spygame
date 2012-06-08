299beb1a5c4780aa9407e3174bc337969f144ba1
Eitan Mosenkis <eitan@mosenkis.net>
2012-06-08 14:19:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpyGame::Application.config.secret_token = '7b33b496fa709799896a214063b80ede7af37a87aeb916a5e9d52aeecbe9a0dc579ccb261f319f86bbb3955ea70a6f84f0e3c6254c8fecd6eaa26d65ba3de2cc'
